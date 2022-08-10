91df7e083cbf16b860fe534fe87e11c15324a82f
Behzod <behzodme@gmail.com>
2022-08-10 19:56:46 +0500

    abu

--- sendsms.py
+++ sendsms.py
@@ -1,5 +1,5 @@
 def sendTelegram(xabar):
     import requests
     api="5429358928:AAE8UcYw8MzTALJLdFZGv_I-6hml1dcUXk8v"
-    url=f'https://api.telegram.org/bot{api}/sendMessage?chat_id=1990786081&text={xabar}'
+    url=f'https://api.telegram.org/bot5429358928:AAE8UcYw8MzTALJLdFZGv_I-6hml1dcUXk8/sendMessage?chat_id=1990786081&text={xabar}'
     requests.get(url)

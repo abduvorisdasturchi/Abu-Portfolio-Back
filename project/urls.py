8e9d87bffd45d19796f3615c2ccafbfce0a88407
Behzod <behzodme@gmail.com>
2022-08-10 19:30:39 +0500

    abu

--- project/urls.py
+++ project/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('behzodadmin/owner/', admin.site.urls),
+    path('abu/owner/', admin.site.urls),
     path('api/v1/',include('app.urls'))
 ]
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
